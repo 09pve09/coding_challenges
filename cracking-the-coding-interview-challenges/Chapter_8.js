8b9f1a4695522dadda769b7a333e68e2abbcdec1
zoharsanchez <lorena.zohar@gmail.com>
2016-10-25 20:40:23 -0700

    Add chapters 2 - 17 and add problem names for chapter 8

---
+++ cracking-the-coding-interview-challenges/Chapter_8.js
@@ -0,0 +1,75 @@
+// Chapter 8 - Recursion and Dynamic Programming
+
+/*
+8.1
+Triple Step: A child is running up a staircase with n steps and hop either 1 step, 2 steps, or 3 steps at a time. Implement a method to count how many possible ways the chlid can run up the stairs.
+ */
+
+const tripleStep = steps => {
+	// Write your code here!
+};
+
+/*
+8.2
+Robot in a grid: Imagine a robot sitting on th eupper left corner of grid with r rows and c columns. The roboto can only move in two diections, right and down, but certain cells are 'off limits' such that the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to the bottom right.
+ */
+
+/*
+8.3
+Magic Index:
+ */
+
+/*
+8.4
+Power Set:
+ */
+
+/*
+8.5
+Recursive Multiply:
+ */
+
+/*
+8.6
+Towers of Hanoi:
+ */
+
+/*
+8.7
+Permutations without Dups:
+ */
+
+/*
+8.8
+Permutations with Dups:
+ */
+
+/*
+8.9
+Parens:
+ */
+
+/*
+8.10
+Paint Fill:
+ */
+
+/*
+8.11
+Coins:
+ */
+
+/*
+8.12
+Eight Queens:
+ */
+
+/*
+8.13
+Stack of Boxes:
+ */
+
+/*
+8.14
+Boolean Evaluations:
+ */
