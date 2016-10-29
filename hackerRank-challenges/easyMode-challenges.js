4f5ce54eb3605e58a76740dbf775bae0a095723f
zoharsanchez <lorena.zohar@gmail.com>
2016-10-28 22:07:45 -0700

    Complete ransomNote in O(n) time

--- hackerRank-challenges/easyMode-challenges.js
+++ hackerRank-challenges/easyMode-challenges.js
@@ -45,4 +45,91 @@ var input = [
 ];
 
 
-console.log(diagonalDiff(input, 3));
+// console.log(diagonalDiff(input, 3));
+
+/*
+Hash Tables: Ransom Note
+A kidnapper wrote a ransom note but is worried it will be traced back to him. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use whole words available in the magazine, meaning he cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+Input Format
+
+The first line contains two space-separated integers describing the respective values of  (the number of words in the magazine) and  (the number of words in the ransom note). 
+The second line contains  space-separated strings denoting the words present in the magazine. 
+The third line contains  space-separated strings denoting the words present in the ransom note.
+
+Constraints
+
+.
+Each word consists of English alphabetic letters (i.e.,  to  and  to ).
+The words in the note and magazine are case-sensitive.
+Output Format
+
+Print Yes if he can use the magazine to create an untraceable replica of his ransom note; otherwise, print No.
+
+Sample Input
+
+6 4
+give me one grand today night
+give one grand today
+Sample Output
+
+Yes
+Explanation
+
+All four words needed to write an untraceable replica of the ransom note are present in the magazine, so we print Yes as our answer.
+ */
+
+var ransomNote = (magazine, ransom) => {
+	magazine = magazine.split(' ');
+	ransom = ransom.split(' ');
+
+	var wordCount = array => {
+		var wordObj = {};
+		for (var i = 0; i < array.length; i++) {
+			if (!wordObj[array[i]]) {
+				wordObj[array[i]] = 1;
+			} else {
+				wordObj[array[i]]++;
+			}
+		}
+		return wordObj;
+	}
+
+	var magazineCounter = wordCount(magazine);
+	var ransomCounter = wordCount(ransom);
+
+	for (var count in ransomCounter) {
+		if (magazineCounter[count] < ransomCounter[count]) {
+			return 'No';
+		}
+	}
+	return 'Yes';
+}
+
+// console.log(ransomNote('give me one grand today night', 'give one grand today')); // Yes
+// console.log(ransomNote('I will shoot that vase', 'I will shoot that vase and then shoot your garden gnome')); // No
+console.log(ransomNote('let us get crunk tonight', 'crunk get crunk')); // No
+// console.log(ransomNote('let us get crunk tonight', 'get crunk')); // Yes
+
+/*
+function counter(a) {
+      var A = {};
+       for (var i=0; i< a.length; i++) {
+         (A[a[i]]) ? A[a[i]] += 1 : A[a[i]] = 1;
+       }
+      return A;
+    }
+    
+    // Main function 
+    magCounter = counter(magazine);
+    ransomCounter = counter(ransom);
+    for (var i in ransomCounter) {
+        if (magCounter[i] < ransomCounter[i]) {
+            console.log('No');
+            return;
+        }
+    }
+    console.log('Yes');
+ */
